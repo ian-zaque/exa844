bdb6051b90ff29275608ad69f6d106dc7f94ac6c
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2023-03-06 17:58:55 -0300

    SaxParser mais correto

--- SaxParser.py
+++ SaxParser.py
@@ -7,23 +7,26 @@ class Listener(xml.sax.ContentHandler):
     self.lat = ""
     self.lon = ""
     self.type = ""
+    self.passedAmenity = False
 
   def startElement(self, tag, attributes):    
     if not self.isAllSet():
       if tag == "node":
+        self.passedAmenity = False
         self.UID = attributes.get("id")
         self.lat = attributes.get("lat")
         self.lon = attributes.get("lon")
         
-      if tag == "tag": 
+      if tag == "tag":
         if attributes.get("k") == "amenity":
+          self.passedAmenity = True
           self.type = attributes.get("v")
 
-        if attributes.get("k") == "name":
+        if attributes.get("k") == "name" and self.passedAmenity == True:
           self.name = attributes.get("v")
     
   def endElement(self, tag):
-    if tag == "tag" and self.isAllSet():
+    if tag == "tag" and self.isAllSet() and self.passedAmenity == True:
       print("Nome: ", self.name)
       print("ID: ", self.UID)
       print("Tipo: ", self.type) 
@@ -33,6 +36,7 @@ class Listener(xml.sax.ContentHandler):
 
   def characters(self, content):
     if self.isAllSet():
+      self.passedAmenity = False
       self.name = ""
       self.UID = ""
       self.lat = ""
@@ -40,7 +44,7 @@ class Listener(xml.sax.ContentHandler):
       self.type = ""
 
   def isAllSet(self):
-    return self.name != "" and self.UID != "" and self.type != "" and self.lat != "" and self.lon != ""
+    return not (self.name == "" or self.UID == "" or self.type == "" or self.lat == "" or self.lon == "")
 
 parser =  xml.sax.make_parser()
 
